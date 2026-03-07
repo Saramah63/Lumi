21eeb5c3bbcc2e23ed70ee1d688c00f1f3fe0762
Sara Mahmodi <saramah63@gmail.com>
2026-03-07 16:17:21 +0200

    Fix invalid Vercel runtime config

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("0d6991888f1b8187")
+/******/ 		__webpack_require__.h = () => ("39865ad606cca7ad")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
