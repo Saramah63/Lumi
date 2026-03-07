d6e80065af68819af467c93a92b738bf4f8b7f63
Sara Mahmodi <saramah63@gmail.com>
2026-03-07 15:55:19 +0200

    Fix Vercel runtime configuration

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1dcc0717a53a03ca"; }
+/******/ 		__webpack_require__.h = function() { return "baede8fa1d089fb4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
